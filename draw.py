e70a6e8d6eb2ec47fcd741ffe4449db7717b6004
Kilio1 <hyuri.gc@gmail.com>
2024-03-03 17:38:20 -0300

    Co-authored-by: Pedro Correia <PedroCorreia73@users.noreply.github.com>

---
+++ draw.py
@@ -0,0 +1,17 @@
+from jogo import FONT, WIN, NAVE, PLAYER_VEL, COVID
+import pygame
+
+def draw(player, elapsed_time, obstaculos):
+    time_text = FONT.render(f"Tempo: {round(elapsed_time)}s", 1, "white")
+    WIN.blit(time_text, (10, 10))
+
+    #rotação da nave e carregamento da imagem na tela
+    NAVE2 = pygame.transform.rotate(NAVE, -PLAYER_VEL * 10)
+    WIN.blit(NAVE2, (player.x, player.y))
+
+    # pygame.draw.rect(WIN, "green", player)
+
+    for obstaculo in obstaculos:
+        # pygame.draw.rect(WIN, "yellow", obstaculo)
+        WIN.blit(COVID, (obstaculo.x, obstaculo.y))
+    pygame.display.update()
