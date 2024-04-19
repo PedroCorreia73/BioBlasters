0237ef76470795a4481543d00c63802c30be54ed
Pedro Correia <pedro.tukinho@gmail.com>
2024-04-19 03:30:49 -0300

    adicionando a conexão ao banco de dados

---
+++ banco _de_dados/professor.py
@@ -0,0 +1,12 @@
+from conexao_banco_de_dados import consultar
+
+class Professor:
+    @consultar
+    def ver_professores(cls):
+        obter_perguntas = "SELECT * FROM professor"
+        cls.consulta.execute(obter_perguntas)
+        resultado = cls.consulta.fetchall()
+        return resultado
+
+    def adicionar_professor(nome_professor, senha_professor):
+        pass
