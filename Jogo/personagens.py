49afb29b275c734ce8bd592e4019e0fe5133d829
Kilio1 <hyuri.gc@gmail.com>
2024-05-03 20:34:03 -0300

    Camadas da tela de perguntas ajustadas

---
+++ Jogo/personagens.py
@@ -0,0 +1,20 @@
+import pygame
+import random
+def criar_item_pergunta():
+    chance = random.randint(0, 10)
+    
+
+def criar_inimigos():
+    INIMIGOS = pygame.image.load("imagens/sprite_enemy.png")
+    INIMIGOS = pygame.transform.scale(INIMIGOS, (50, 50))
+    return INIMIGOS
+
+def criar_nave(PLAYER_WIDTH, PLAYER_HEIGHT):
+    NAVE_imagem = pygame.image.load("imagens/sprites_ship.png")
+    NAVE = pygame.transform.scale(NAVE_imagem, (PLAYER_WIDTH + 10, PLAYER_HEIGHT + 10))
+    return NAVE
+
+def gerar_personagens(PLAYER_WIDTH, PLAYER_HEIGHT):
+    INIMIGOS = criar_inimigos()
+    NAVE = criar_nave(PLAYER_WIDTH, PLAYER_HEIGHT)
+    return INIMIGOS, NAVE
---
+++ Jogo/tela.py
@@ -0,0 +1,33 @@
+import pygame
+
+def tela():
+    pygame.font.init() #inicializar o font module (sem isso não dá para usar fontes)
+    pygame.display.init() # inicializar a tela (serve para poder obter as medidas da tela do usuário)
+    #WIDTH, HEIGHT = pygame.display.get_desktop_sizes()[0] #obtém as medidas da tela do usuário
+    WIDTH, HEIGHT = 1200, 600
+    WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption("BioBlasters")
+    FONT = pygame.font.SysFont("comicsans", 30) #tipo e tamanho da fonte estocada na variável FONT
+    return WIN, FONT
+
+def draw(player, elapsed_time, obstaculos, itenspergunta, FONT, WIN, NAVE, PLAYER_VEL, COVID, hit_itempergunta):
+    time_text = FONT.render(f"Tempo: {round(elapsed_time)}s", 1, "white")
+    WIN.blit(time_text, (10, 10))
+
+    #rotação da nave e carregamento da imagem na tela
+    NAVE2 = pygame.transform.rotate(NAVE, -PLAYER_VEL * 10)
+    WIN.blit(NAVE2, (player.x - 10, player.y - 10))
+
+    #pygame.draw.rect(WIN, "green", player) #hitbox do player
+    for obstaculo in obstaculos:
+        #if hit_itempergunta is True and origem_plano_resposta[0] < obstaculo.x < origem_plano_resposta[0] + bgp.get_width() and origem_plano_resposta[1] < obstaculo.y and origem_plano_resposta[1] + bgp.get_height():
+       #     pass
+        #else:
+            #pygame.draw.rect(WIN, "yellow", obstaculo) #hitbox dos obstáculos
+            WIN.blit(COVID, (obstaculo.x - 8, obstaculo.y -8))
+
+    for itempergunta in itenspergunta:
+        pygame.draw.rect(WIN, "blue", itempergunta) #hitbox dos obstáculos
+        #WIN.blit(COVID, (itempergunta.x - 8, itempergunta.y -8))
+    if not hit_itempergunta:
+        pygame.display.update()
